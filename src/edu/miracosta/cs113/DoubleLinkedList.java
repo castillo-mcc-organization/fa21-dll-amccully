bc9e9b1c520b12a3938f3255d0442420e7f90d8c
amccully <amccully2001@gmail.com>
2021-09-28 17:52:43 -0700

    Added Iterator (mostly)

--- src/edu/miracosta/cs113/DoubleLinkedList.java
+++ src/edu/miracosta/cs113/DoubleLinkedList.java
@@ -1,20 +1,22 @@
 package edu.miracosta.cs113;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
-public class DoubleLinkedList<E> implements List {
-    Node<E> head;
-    public DoubleLinkedList(int i) {
-        if(i < 0 || i > size) {
-            throw new IndexOutOfBoundsException("Invalid index: " + i);
-        }
+public class DoubleLinkedList<E> implements List<E> {
+    // data fields
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
+
+    public DoubleLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
     }
 
+    @Override
     public String toString() {
-        Node<String> nodeRef = head;
+        Node<E> nodeRef = head;
         String result = "";
         while(nodeRef != null) {
             result += nodeRef.data;
@@ -35,24 +37,162 @@ public class DoubleLinkedList<E> implements List {
 
         private Node(E dataItem) {
             data = dataItem;
+            ///
         }
     }
 
     /*
         add docs
      */
-    private class DoubleListIterator {
+    private class DoubleListIterator implements ListIterator<E> {
+        private Node<E> nextItem;
+        private Node<E> lastItemReturned;
+        private int index;
 
+        public DoubleListIterator(int i) {
+            if (i < 0 || i > size) {
+                throw new IndexOutOfBoundsException("Invalid index " + i);
+            }
+            lastItemReturned = null;
+            if (i == size) {
+                index = size;
+                nextItem = null;
+            } else {
+                nextItem = head;
+                for (index = 0; index < i; index++) {
+                    nextItem = nextItem.next;
+                }
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (nextItem != null);
+        }
+
+        @Override
+        public E next() {
+            if(!hasNext()) {
+                throw new NoSuchElementException("No next element");
+            }
+            lastItemReturned = nextItem;
+            nextItem = nextItem.next;
+            index++;
+            return lastItemReturned.data;
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            return ((nextItem == null && size != 0) || nextItem.prev != null);
+        }
+
+        @Override
+        public E previous() {
+            if(!hasPrevious()) {
+                throw new NoSuchElementException("No previous element");
+            }
+            if(nextItem == null) {
+                nextItem = tail;
+            } else {
+                nextItem = nextItem.prev;
+            }
+            lastItemReturned = nextItem;
+            index--;
+            return lastItemReturned.data;
+        }
+
+        @Override
+        public int nextIndex() {
+            if(hasNext()) {
+                return index;
+            }
+            return -1;
+        }
+
+        @Override
+        public int previousIndex() {
+            if(hasPrevious()) {
+                if (nextItem == null) {
+                    return (size - 1);
+                } else {
+                    return (index - 1);
+                }
+            }
+            return -1;
+        }
+
+        @Override
+        public void remove() {
+            if(lastItemReturned == null) {
+                throw new IllegalStateException("No last element returned");
+            }
+            if(tail == lastItemReturned) {
+                tail.prev.next = null;
+                tail = tail.prev;
+            }
+            else if(head == lastItemReturned) {
+                if(head.next == null) {
+                    head = null;
+                    tail = null;
+                }
+                else {
+                    head = head.next;
+                    head.prev = null;
+                }
+            }
+            else {
+                lastItemReturned.prev.next = lastItemReturned.next;
+                lastItemReturned.next.prev = lastItemReturned.prev;
+            }
+            size--;
+            index--; //?
+            lastItemReturned = null;
+        }
+
+        @Override
+        public void set(E o) {
+
+        }
+
+        @Override
+        public void add(E o) {
+            if(head == null) {  // adding to an empty list
+                head = new Node<E>(o);
+                tail = head;
+            }
+            else if(nextItem == head) {  // adding to the head of the list
+                Node<E> nodeRef = new Node<E>(o);
+                nodeRef.next = head;
+                head.prev = nodeRef;
+                head = nodeRef;
+            }
+            else if(nextItem == null) {  // since we already checked for list being empty, we only need to see if nextItem is null
+                Node<E> nodeRef = new Node<E>(o);
+                nodeRef.prev = tail;
+                tail.next = nodeRef;
+                tail = nodeRef;
+            }
+            else {
+                Node<E> nodeRef = new Node<E>(o); //*
+                nodeRef.next = nextItem; //*
+                nodeRef.prev = nextItem.prev; //*
+                nextItem.prev.next = nodeRef; //*
+                nextItem.prev = nodeRef; //*
+            }
+            size++;
+            index++;
+            lastItemReturned = null;
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return (head == null);
     }
 
     @Override
@@ -71,7 +211,7 @@ public class DoubleLinkedList<E> implements List {
     }
 
     @Override
-    public boolean add(Object o) {
+    public boolean add(E o) {
         return false;
     }
 
@@ -96,7 +236,7 @@ public class DoubleLinkedList<E> implements List {
     }
 
     @Override
-    public Object get(int i) {
+    public E get(int i) {
         return null;
     }
 
@@ -106,12 +246,12 @@ public class DoubleLinkedList<E> implements List {
     }
 
     @Override
-    public void add(int i, Object o) {
-
+    public void add(int i, E obj) {
+        listIterator(i).add(obj);
     }
 
     @Override
-    public Object remove(int i) {
+    public E remove(int i) {
         return null;
     }
 
